bcfc78a6dfe2f95fc1d02aef706ad75ecd5f670f
Anmol Thukral <thukral.anmol21@gmail.com>
2026-01-30 00:31:22 +0530

    chore: test

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,5 +1,9 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    'engineering-playbook-nav': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+import React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'engineering-playbook-nav': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    }
   }
 }
