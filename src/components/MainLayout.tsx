afda5279a2c0750944f1468ab20154f9559daa85
Anmol Thukral <thukral.anmol21@gmail.com>
2026-01-29 23:37:40 +0530

    fix: navigation

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -3,10 +3,7 @@ import React from 'react';
 const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 text-gray-900 font-sans">
-      {/* 
-          @ts-ignore: Web Component 
-          The navbar is loaded from common-nav microfrontend
-      */}
+      {/* The navbar is loaded from common-nav microfrontend */}
       <engineering-playbook-nav></engineering-playbook-nav>
       
       <main className="flex-grow max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-8 mt-16">
