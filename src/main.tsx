bcea65f60091ca7dcd5281e7fd523f790ea2d2cd
Anmol Thukral <thukral.anmol21@gmail.com>
2026-01-30 18:47:49 +0530

    fix: tutorials

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+type ImportMetaEnvShape = { BASE_URL?: string };
+const BASE = (import.meta as unknown as { env?: ImportMetaEnvShape }).env?.BASE_URL ?? '/tutorials/';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={BASE}>
       <App />
     </BrowserRouter>
   </StrictMode>,
