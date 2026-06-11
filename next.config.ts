3080b5d9b467b09f45329f042d19f3c8875a3314
Anmol Thukral <thukral.anmol21@gmail.com>
2026-06-11 16:51:13 +0530

    chore: fix ui

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/tutorials',
-  assetPrefix: '/tutorials/',
   trailingSlash: true,
   reactStrictMode: true,
   turbopack: { root: path.resolve('../..') },
